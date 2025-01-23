7b9fea47dcac3266f3dc658a6da4b88289f33f59
Franek <hubert.krzysciak@microsoft.wsei.edu.pl>
2025-01-23 22:14:30 +0100

    commit3

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,27 +1,66 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+using WebApplication3.Models.Services;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+namespace WebApplication3;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddControllersWithViews();
+        ///////////////////////////
+        builder.Services.AddDbContext<AppDbContext>();
+
+        builder.Services.AddDbContext<MoviesDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("MoviesDb")));
+
+
+        
+        builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                }
+                )
+                .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>();
+        builder.Services.AddTransient<IContactService, EFContactService>();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddSession();
+        ///////////////////////////
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseSession();
+
+        app.MapRazorPages();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
